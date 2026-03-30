550217a3e87c17cdb6ddb5096c0b2891e252a447
meruertumarova2206-web <meruertumarova2206@gmail.com>
2026-03-30 19:43:18 +0500

    магазин одежды

--- src/main/java/org/example/five.java
+++ src/main/java/org/example/five.java
@@ -2,19 +2,19 @@ package org.example;
 import java.util.Scanner;
 public class five {
 
-    public static void printRev(int n, Scanner scanner) {
+    public static void sum(int n, Scanner scanner) {
         if (n <= 0) {
-            return;
+            return ;
         }
         int x = scanner.nextInt();
-        printRev(n - 1, scanner);
+        sum(n - 1, scanner);
         System.out.print(x + " ");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        printRev(n, scanner);
+        sum(n, scanner);
 
         scanner.close();
     }
