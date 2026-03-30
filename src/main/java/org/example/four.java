00da2f13b03746fc8b63ee8fbcaa6b720ca2a68d
meruertumarova2206-web <meruertumarova2206@gmail.com>
2026-03-30 19:40:54 +0500

    магазин одежды

--- src/main/java/org/example/four.java
+++ src/main/java/org/example/four.java
@@ -1,23 +1,21 @@
 package org.example;
 import java.util.Scanner;
 public class four{
-    public static int sum(int[] arr, int n) {
+    public static int sum(int[] arr, int n){
         if(n<=0){
             return 0;
         }
-        int last = arr[n - 1] > 0 ? arr[n - 1] : 0;
-        return last + sum(arr, n - 1);
+        int last = arr[n-1] >0 ? arr[n-1] :0;
+        return last+sum(arr,n-1);
     }
-
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int[] numbers = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            numbers[i] = scanner.nextInt();
+        for(int i=0;i<size;i++) {
+            numbers[i]=scanner.nextInt();
         }
-        int result = sum(numbers, size);
+        int result = sum(numbers,size);
         System.out.println(result);
         scanner.close();
     }
