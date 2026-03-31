bd7157a8d27cdfe1a44161f10f2788204170a8c2
meruertumarova2206-web <meruertumarova2206@gmail.com>
2026-03-31 08:40:59 +0500

    магазин одежды

--- src/main/java/org/example/eight.java
+++ src/main/java/org/example/eight.java
@@ -2,23 +2,23 @@ package org.example;
 import java.util.Scanner;
 public class eight {
 
-    public static void sum(int[][] matrix, int start, int end, int num) {
-        if (start > end) {
+    public static void sum(int[][] matrix,int start,int end,int num){
+        if(start>end) {
             return;
         }
-        for (int i = start; i <= end; i++) {
+        for(int i=start;i<=end;i++) {
             matrix[start][i] = num++;
         }
-        for (int i = start + 1; i <= end; i++) {
+        for(int i= start+1;i<=end;i++) {
             matrix[i][end] = num++;
         }
-        for (int i = end - 1; i >= start; i--) {
-            matrix[end][i] = num++;
+        for(int i = end-1; i>=start; i--){
+            matrix[end][i]=num++;
         }
-        for (int i = end - 1; i > start; i--) {
-            matrix[i][start] = num++;
+        for(int i=end-1;i>start;i--){
+            matrix[i][start]=num++;
         }
-        sum(matrix, start + 1, end - 1, num);
+        sum(matrix, start+1, end-1, num);
     }
 
     public static void main(String[] args) {
