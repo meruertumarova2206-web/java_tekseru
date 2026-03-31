95eb36a2f6b35c3831020c5861e1f8f65e784ba0
meruertumarova2206-web <meruertumarova2206@gmail.com>
2026-03-31 09:05:17 +0500

    магазин одежды

--- src/main/java/org/example/eight.java
+++ src/main/java/org/example/eight.java
@@ -1,30 +1,28 @@
 package org.example;
 import java.util.Scanner;
 public class eight {
-
-    public static void sum(int[][] matrix,int start,int end,int num){
-        if(start>end) {
+    public static void sum(int[][] matrix,int start, int end, int num) {
+        if (start > end) {
             return;
         }
-        for(int i=start;i<=end;i++) {
+        for (int i = start; i <= end; i++) {
             matrix[start][i] = num++;
         }
-        for(int i= start+1;i<=end;i++) {
+        for (int i = start + 1; i <= end; i++) {
             matrix[i][end] = num++;
         }
-        for(int i = end-1; i>=start; i--){
-            matrix[end][i]=num++;
+        for (int i = end - 1; i >= start; i--) {
+            matrix[end][i] = num++;
         }
-        for(int i=end-1;i>start;i--){
-            matrix[i][start]=num++;
+        for (int i = end - 1; i > start; i--) {
+            matrix[i][start] = num++;
         }
-        sum(matrix, start+1, end-1, num);
+        sum(matrix, start + 1, end - 1, num);
     }
-
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
-        int N= scanner.nextInt();
-        int[][] matrix= new int[N][N];
+        int N = scanner.nextInt();
+        int[][] matrix = new int[N][N];
         sum(matrix,0,N-1,1);
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
