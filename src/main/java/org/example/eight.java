82ac3e18c3e51a63c65f9220d99a558d838a408f
meruertumarova2206-web <meruertumarova2206@gmail.com>
2026-03-31 08:16:15 +0500

    магазин одежды

--- src/main/java/org/example/eight.java
+++ src/main/java/org/example/eight.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.Scanner;
 public class eight {
 
-    public static void fillSpiral(int[][] matrix, int start, int end, int num) {
+    public static void sum(int[][] matrix, int start, int end, int num) {
         if (start > end) {
             return;
         }
@@ -18,14 +18,14 @@ public class eight {
         for (int i = end - 1; i > start; i--) {
             matrix[i][start] = num++;
         }
-        fillSpiral(matrix, start + 1, end - 1, num);
+        sum(matrix, start + 1, end - 1, num);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int[][] matrix = new int[N][N];
-        fillSpiral(matrix, 0, N - 1, 1);
+        sum(matrix, 0, N - 1, 1);
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 System.out.print(matrix[i][j] + " ");
